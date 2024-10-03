d717c4016c4510e9b575eaffd111dbe03ad7520e
Кузьминых Сергей <kuzminyhsa@gmail.com>
2024-10-03 14:51:54 +0800

    Add logging and other

--- src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
@@ -5,45 +5,47 @@ import jm.task.core.jdbc.util.Util;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
-import org.hibernate.query.NativeQuery;
+//import org.hibernate.query.NativeQuery;
 
 import javax.persistence.Query;
 import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class UserDaoHibernateImpl implements UserDao {
-
+    private final String  CREATE_TABLE = "CREATE TABLE IF NOT EXISTS `user` ( `id` BIGINT(5) NOT NULL AUTO_INCREMENT, `name` VARCHAR(45) NOT NULL,`lastName` VARCHAR(45) NOT NULL, `age` TINYINT(3) NULL,  PRIMARY KEY (`id`))  ENGINE = InnoDB  DEFAULT CHARACTER SET = utf8;";
+    private final String  DROP_TABLE = "DROP TABLE IF EXISTS `user`;";
+    private static final Logger logger = Logger.getLogger("UserDaoHibernateImpl.class");
     SessionFactory sessionFactory = Util.getSessionFactory();
 
     public UserDaoHibernateImpl() {
 
     }
 
-
     @Override
     public void createUsersTable() {
-        String sql = "        CREATE TABLE IF NOT EXISTS `user` (\n" +
-                "  `id` BIGINT(5) NOT NULL AUTO_INCREMENT,\n" +
-                "  `name` VARCHAR(45) NOT NULL,\n" +
-                " `lastName` VARCHAR(45) NOT NULL,\n" +
-                "  `age` TINYINT(3) NULL,\n" +
-                "                PRIMARY KEY (`id`))\n" +
-                "        ENGINE = InnoDB\n" +
-                "        DEFAULT CHARACTER SET = utf8;";
-        try (Session session = sessionFactory.openSession()) {
+        Session session = null;
+        try {
+            session = sessionFactory.openSession();
             Transaction transaction = session.beginTransaction();
-            Query query = session.createSQLQuery(sql).addEntity(User.class);
+            Query query = session.createSQLQuery(CREATE_TABLE);
             query.executeUpdate();
             transaction.commit();
+        } catch (Exception e) {
+            logger.log(Level.WARNING, "Error create table");
+        } finally {
+            if (session != null && session.isOpen()) {
+                session.close();
+            }
         }
-
     }
 
     @Override
     public void dropUsersTable() {
-        String sql = "DROP TABLE IF EXISTS `user`;";
+
         try (Session session = sessionFactory.openSession()) {
             Transaction transaction = session.beginTransaction();
-            Query query = session.createSQLQuery(sql).addEntity(User.class);
+            Query query = session.createSQLQuery(DROP_TABLE);
             query.executeUpdate();
             transaction.commit();
         }
@@ -61,7 +63,8 @@ public class UserDaoHibernateImpl implements UserDao {
             if (transaction != null) {
                 transaction.rollback();
             }
-            e.printStackTrace();
+            logger.log(Level.WARNING, "Error save user");
+//            e.printStackTrace();
         }
 
     }
@@ -71,23 +74,26 @@ public class UserDaoHibernateImpl implements UserDao {
         Transaction transaction = null;
         try ( Session session = sessionFactory.openSession()) {
             transaction = session.beginTransaction();
-            session.delete(session.get(User.class, id));
+            User user =  session.get(User.class, id);
+            if(user !=null){
+                session.delete(user);
+            }
             transaction.commit();
         } catch (Exception e) {
             if (transaction != null) {
                 transaction.rollback();
             }
-            e.printStackTrace();
+            logger.log(Level.WARNING, "Error remove user");
+            //e.printStackTrace();
         }
     }
 
     @Override
     public List<User> getAllUsers() {
-        String sql = "SELECT * FROM USER";
         Transaction transaction = null;
         try(Session session = sessionFactory.openSession()) {
              transaction = session.beginTransaction();
-            Query query = session.createNativeQuery(sql).addEntity(User.class);
+            Query query = session.createQuery("from User ");
             List<User> userList = query.getResultList();
             transaction.commit();
             return userList;
@@ -96,7 +102,8 @@ public class UserDaoHibernateImpl implements UserDao {
             if (transaction != null) {
                 transaction.rollback();
             }
-            e.printStackTrace();
+            logger.log(Level.WARNING, "Error get all users");
+           // e.printStackTrace();
         }
         return null;
     }
@@ -113,7 +120,8 @@ public class UserDaoHibernateImpl implements UserDao {
             if (transaction != null) {
                 transaction.rollback();
             }
-            e.printStackTrace();
+            logger.log(Level.WARNING, "Error clean table");
+           // e.printStackTrace();
         }
     }
 }
